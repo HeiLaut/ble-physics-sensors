10e3703122744ebcd14e7ce9b8473cfd0c171e69
HeiLaut <heinrichgeske@gmail.com>
2022-03-11 12:14:54 +0100

    esp32 voltmeter
    changed to work with esp32

--- Arduino-files/voltmeter/voltmeter.ino
+++ Arduino-files/voltmeter/voltmeter.ino
@@ -1,4 +1,4 @@
-const int analogInput = 1;
+const int analogInput = 2;
 float Vout = 0.00;
 float Vin = 0.00;
 float R1 = 100000.00; // resistance of R1 (100K) 
@@ -6,12 +6,12 @@ float R2 = 10000.00; // resistance of R2 (10K)
 int val = 0;
 void setup(){
    pinMode(analogInput, INPUT); //assigning the input port
-   Serial.begin(9600); //BaudRate
+   Serial.begin(115200); //BaudRate
 }
 void loop(){
    
    val = analogRead(analogInput);//reads the analog input
-   Vout = (val * 5.00) / 1024.00; // formula for calculating voltage out i.e. V+, here 5.00
+   Vout = (val * 6.64) / 4095; // formula for calculating voltage out i.e. V+, here 5.00
    Vin = Vout / (R2/(R1+R2)); // formula for calculating voltage in i.e. GND
    if (Vin<0.09)//condition 
    {
@@ -19,5 +19,5 @@ void loop(){
   } 
 //Serial.print("\t Voltage of the given source = ");
 Serial.println(Vin);
-delay(5); //for maintaining the speed of the output in serial moniter
+delay(100); //for maintaining the speed of the output in serial moniter
 }
