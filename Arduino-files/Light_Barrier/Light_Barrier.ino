e5b4b6af16626d2d2d244892c2520630fb6fc521
HeiLaut <heinrichgeske@gmail.com>
2022-03-29 21:50:24 +0200

    added time differences

--- Arduino-files/Light_Barrier/Light_Barrier.ino
+++ Arduino-files/Light_Barrier/Light_Barrier.ino
@@ -2,6 +2,14 @@
 
 const int analogInput1 = 36;
 float val1 = 0;
+float t0 = 0;
+float t1 = 0;
+float dt = 0; //darkeningtme
+
+float tdif = 0; //time between two signals
+float tdif0 = 0;
+bool trigger = false;
+
 void setup() {
     PhyphoxBLE::start("Lichtschranke");
     PhyphoxBleExperiment lightBarrier;
@@ -34,9 +42,24 @@ void loop() {
     float t = 0.001 * (float)millis();
 
     val1 = analogRead(analogInput1);//reads the analog input
-    Serial.println(val1);
+    if(val1 > 2000 and !trigger){
+        tdif = t - tdif0;
+        t0 = t;
+        tdif0 = t;
+        trigger = true;
+        Serial.println(tdif);
+        
+    }
+    if(val1 <2000 and trigger){
+      trigger = false;
+      t1 = t;
+      dt = t1-t0;
+      //Serial.println(dt);
+
+    }
+    
     PhyphoxBLE::write(t,val1);
-    delay(10);
+    delay(5);
 
 
 }
