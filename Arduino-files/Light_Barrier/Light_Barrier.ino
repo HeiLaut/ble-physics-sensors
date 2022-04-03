fad49febede76079e712fe0e61b7a7356955c3a4
HeiLaut <heinrichgeske@gmail.com>
2022-04-03 22:49:00 +0200

    simplyfied the file

--- Arduino-files/Light_Barrier/Light_Barrier.ino
+++ Arduino-files/Light_Barrier/Light_Barrier.ino
@@ -2,37 +2,31 @@
 
 const int analogInput1 = 36;
 const int analogInput2 = 39;
-float val1 = 0;
-float val2 = 0;
+float valA = 0;
+float valB = 0;
 
-float t0 = 0;
-float t1 = 0;
-float dt = 0; //darkeningtme
-float tdif = 0; //time between two signals
-float tdif0 = 0;
-bool trigger = false;
-float sig = 0;
+bool trigA = false;
+bool trigB = false;
 
-float t0_2 = 0;
-float t1_2 = 0;
-float dt_2 = 0; //darkeningtme
-float tdif_2 = 0; //time between two signals
-float tdif0_2 = 0;
-bool trigger_2 = false;
-float sig_2 = 0;
+float tAR = 0; //time a raising edge
+float tAF = 0; //time a falling edge
+float dtA = 0; //darkening time
 
-float dt_1_2 = 0;
-bool trigger_1_2 = false;
-bool trigger_2_1 = false;
+float tBR = 0;
+float tBF = 0;
+float dtB = 0;
+
+float dtABR = 0; //time between A and B raising points
+float dtABF = 0; //time between A and B falling points
+
 
 void setup() {
     PhyphoxBLE::start("Lichtschranke");
+    
     PhyphoxBleExperiment lightBarrier;
     
     lightBarrier.setTitle("Lichtschranke");
     lightBarrier.setCategory("Sensor-Boxen");
-
-
     PhyphoxBleExperiment::View graph;
     graph.setLabel("Graph");
 
@@ -56,39 +50,58 @@ void setup() {
     sigGraph2.setLabelX("Zeit t");
     sigGraph2.setLabelY("");
     sigGraph2.setColor("FFCC5C");
-    sigGraph2.setChannel(1,5);
+    sigGraph2.setChannel(1,3);
     
-    PhyphoxBleExperiment::Graph timGraph;
-    timGraph.setLabel("Zeiten");
-    timGraph.setUnitY("s");
-    timGraph.setUnitX("s");
-    timGraph.setLabelX("Laufzeit");
-    timGraph.setLabelY("Verdunklungszeit");
-    timGraph.setColor("FFCC5C");
-    timGraph.setChannel(3,4);
-    timGraph.setStyle("dots");
+//    PhyphoxBleExperiment::Graph timGraph;
+//    timGraph.setLabel("Zeiten");
+//    timGraph.setUnitY("s");
+//    timGraph.setUnitX("s");
+//    timGraph.setLabelX("Laufzeit");
+//    timGraph.setLabelY("Verdunklungszeit");
+//    timGraph.setColor("FFCC5C");
+//    timGraph.setChannel(3,4);
+//    timGraph.setStyle("dots");
 
-    PhyphoxBleExperiment::Value deltaT;
-    deltaT.setLabel("Verdunklungszeit L1 t =");
-    deltaT.setPrecision(3);
-    deltaT.setUnit("s");
-    deltaT.setColor("FFCC5C"); 
-    deltaT.setChannel(3);
-    deltaT.setXMLAttribute("size=\"2\"");
+    PhyphoxBleExperiment::Value deltaTR;
+    deltaTR.setLabel("Signallaufzeit L1 L2 R t =");
+    deltaTR.setPrecision(3);
+    deltaTR.setUnit("s");
+    deltaTR.setColor("FFCC5C"); 
+    deltaTR.setChannel(4);
+    deltaTR.setXMLAttribute("size=\"2\"");
 
-    PhyphoxBleExperiment::Value deltaT0;
-    deltaT0.setLabel("Signallaufzeit L1 L2 t = ");
-    deltaT0.setPrecision(3);
-    deltaT0.setUnit("s");
-    deltaT0.setColor("FFCC5C"); 
-    deltaT0.setChannel(4);
-    deltaT0.setXMLAttribute("size=\"2\"");
+    PhyphoxBleExperiment::Value deltaTF;
+    deltaTF.setLabel("Signallaufzeit L1 L2 F t = ");
+    deltaTF.setPrecision(3);
+    deltaTF.setUnit("s");
+    deltaTF.setColor("FFCC5C"); 
+    deltaTF.setChannel(5);
+    deltaTF.setXMLAttribute("size=\"2\"");
 
-    simple.addElement(deltaT);
-    simple.addElement(deltaT0);
+//    PhyphoxBleExperiment::Value deltaA;
+//    deltaA.setLabel("Verdunklungszeit A t =");
+//    deltaA.setPrecision(3);
+//    deltaA.setUnit("s");
+//    deltaA.setColor("FFCC5C"); 
+//    deltaA.setChannel(6);
+//    deltaA.setXMLAttribute("size=\"2\"");
+//
+//    PhyphoxBleExperiment::Value deltaB;
+//    deltaB.setLabel("Verdunklungszeit B t = ");
+//    deltaB.setPrecision(3);
+//    deltaB.setUnit("s");
+//    deltaB.setColor("FFCC5C"); 
+//    deltaB.setChannel(7);
+//    deltaB.setXMLAttribute("size=\"2\"");
+
+
+    simple.addElement(deltaTR);
+    simple.addElement(deltaTF);
+    //simple.addElement(deltaA);
+   // simple.addElement(deltaB);
     graph.addElement(sigGraph);
     graph.addElement(sigGraph2);
-    graph.addElement(timGraph);
+    //graph.addElement(timGraph);
     lightBarrier.addView(graph);
     lightBarrier.addView(simple);
     PhyphoxBLE::addExperiment(lightBarrier);
@@ -103,66 +116,44 @@ void setup() {
 
 void loop() {
     float t = 0.001 * (float)millis();
-    bool s1 = true;
-    bool s2 = true;
-    val1 = analogRead(analogInput1);//reads the analog input
-    val2 = analogRead(analogInput2);//reads the analog input
 
-    if(val1 < 2000){
-      s1 = false;
-    }
-  
-    if(val2 <2000){
-      s2 = false;
+    valA = analogRead(analogInput1);//reads the analog input
+    valB = analogRead(analogInput2);//reads the analog input
+
+    if(valA > 1000 and !trigA){
+      tAR = t;
+      trigA = true;
+           
     }
 
-    if(val1 > 3100 and !trigger and s1){
-        sig = 1;
-        tdif = t - tdif0;
-        t0 = t;
-        tdif0 = t;
-        trigger = true;
-        trigger_1_2 = true;
-         if(trigger_2_1){
-          dt_1_2 = t - tdif0_2;
-          trigger_2_1 = false;
-          Serial.println(dt_1_2);
-        }
-        //Serial.println(tdif);
-        
-    }
-    if(val1 <3100 and trigger and s1){
-      sig = 0;
-      trigger = false;
-      t1 = t;
-      dt = t1-t0;
+    if(valA < 1000 and trigA){
+      tAF = t;
+      dtA = t - tAR;
+      trigA = false;
     }
 
-    if(val2 > 3200 and !trigger_2 and s2){
-        sig_2 = 1;
-        tdif_2 = t - tdif0_2;
-        t0_2 = t;
-        tdif0_2 = t;
-        trigger_2 = true;
-        trigger_2_1 = true;
-        if(trigger_1_2){
-          dt_1_2 = t - tdif0;
-          trigger_1_2 = false;
-          Serial.println(dt_1_2);
-        }
-        }
-        //Serial.println(tdif);
-        
-    
-    if(val2 <3200 and trigger_2 and s2){
-      sig_2 = 0;
-      trigger_2= false;
-      t1_2 = t;
-      dt_2 = t1_2-t0_2;
+    if(valB > 1000 and !trigB){
+      tBR = t;
+      trigB = true;
+    }
+
+    if(valB < 1000 and trigB){
+      tBF = t;
+      dtB = t - tBR;
+      trigB = false;
     }
     
-    PhyphoxBLE::write(t,val1,dt,dt_1_2,val2);
-    //delay(0);
+    dtABR = abs(tAR-tBR);
+    dtABF = abs(tAF-tBF);
+    
+
+
+
+    PhyphoxBLE::write(t,valA,valB,dtABR,dtABF);//,dtA,dtB);
+   // Serial.print(dtABR);
+   // Serial.print(",");
+   // Serial.println(dtABF);
+    //delay(5);
 
 
 }
