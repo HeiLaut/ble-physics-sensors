df6187a3e61c46a39363e88e603d57da225a9422
HeiLaut <66202132+HeiLaut@users.noreply.github.com>
2026-09-11 22:13:22 +0200

    Update light_barrier_single_oled.ino

--- Light Gate/Arduino/light_barrier_single_oled/light_barrier_single_oled.ino
+++ Light Gate/Arduino/light_barrier_single_oled/light_barrier_single_oled.ino
@@ -4,8 +4,23 @@
  * Author: Heinrich Lauterbach
  * GitHub: https://github.com/HeiLaut/ble-physics-sensors
  *
- * License (own code): CC BY-SA 4.0
- * https://creativecommons.org/licenses/by-sa/4.0/deed.en
+ * License (own code): MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, subject to the following condition:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * Libraries used:
  * - phyphoxBLE (LGPL 3.0) - https://github.com/phyphox/phyphox-arduino
